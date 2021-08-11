4cd9c001de25f5cd9af21a696cfcbfe544c0053f
Juraj Puchký <jpuchky@paralix.baba.bj>
2021-08-11 19:35:53 +0200

    future keywords

--- samples/kgr-csv.php
+++ samples/kgr-csv.php
@@ -10,8 +10,9 @@ use BABA\Search\Engines\Google;
 $engine = new Google();
 $cache = new \BABA\Cache\Cache(new \BABA\Cache\Drivers\Disk());
 $list = [];
+$potentialy = [];
 $volumes = [];
-if($argc > 2) {
+if ($argc > 3) {
     $fp = fopen($argv[1], 'r');
     $head = fgetcsv($fp, 10000, ";");
     $keywords = [];
@@ -20,7 +21,7 @@ if($argc > 2) {
     }
     fclose($fp);
     foreach ($keywords as $keyword => $volume) {
-        if($volume <= 250) {
+        if ($volume <= 250) {
             echo "Checking $keyword ($volume)...";
             $results = (new Results($engine, $cache))->getResult($keyword, 'lang_cs', ['cs'], ['allintitle' => true]);
             if (KGR::isKgr($results, $volume)) {
@@ -33,6 +34,7 @@ if($argc > 2) {
                 }
             } else {
                 if($volume == 0) {
+                    $potentialy[] = $keyword;
                     echo "Potentialy future kgr\n";
                 } else {
                     echo "Not kgr\n";
@@ -45,16 +47,24 @@ if($argc > 2) {
 
     $t = 0;
     $content = "keyword;kgr;volume\n";
-    foreach($list as $keyword => $kgr) {
-        $content.= "$keyword;$kgr;".$volumes[$keyword]."\n";
-        if($kgr == 0) {
+    foreach ($list as $keyword => $kgr) {
+        $content .= "$keyword;$kgr;" . $volumes[$keyword] . "\n";
+        if ($kgr == 0) {
             $t += $volumes[$keyword];
         } else {
             $t += (1 / $kgr) * $volumes[$keyword];
         }
-     }
-    file_put_contents($argv[2],$content);
+    }
+    file_put_contents($argv[2], $content);
     echo "Totally potential traffic $t\n";
+    $t = 0;
+    $content = "keyword\n";
+    foreach ($potentialy as $keyword) {
+        $content .= "$keyword\n";
+        $t++;
+    }
+    file_put_contents($argv[3], $content);
+    echo "Found {$t} future keywords\n";
 } else {
-    echo "Usage: php kgr-csv.php <csv file> <out csv file>\n";
+    echo "Usage: php kgr-csv.php <csv file> <out csv file> <out potentialy csv>\n";
 }
